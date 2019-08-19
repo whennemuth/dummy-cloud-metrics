5db13dd69ee50fc0e72b44c9d3f2a83926abd68a
Hennemuth <wrh@bu.edu>
2019-08-18 20:29:48 -0400

    Each instance divided into 3 separate tabs

--- src/Instance.js
+++ src/Instance.js
@@ -4,6 +4,7 @@ import { ContainerInstanceLookup } from './Lookup'
 import TabStrip from './Tab'
 import JsonToTable from './TabularView'
 import Metrics from './Metric'
+import { tsImportEqualsDeclaration } from '@babel/types';
 
 const Heading = ({title}) => { return (<div className="separator">{title}</div>) };
 
@@ -17,7 +18,7 @@ class Instances extends Component {
             query: null
         })
     
-        // The first tab is for fake metric substitutions (each remaining tab represents its own container instance)
+        // The first tab represents all instances in the cluster collectively. Each remaining tab represents an individual instance.
         this.state.tabdefs.push({
             data:"metrics-config",
             label:"CONFIG",
@@ -45,25 +46,29 @@ class Instances extends Component {
         return defs
     }
 
+    getData = () => {
+        let activeTabdef = this.state.tabdefs.find(tabdef =>{
+            if(tabdef.active) {
+                return tabdef
+            }
+        })
+        // All tabs except for the first are for container instance information
+        return activeTabdef.data == "metrics-config" ? "CLUSTER" : activeTabdef.data
+    }
+
     render() {
         let currentTab = null;
         this.state.tabdefs.forEach(tabdef => {
             if(tabdef.active) {
-                // All tabs except for the first are for container instance information
-                if(tabdef.data == "metrics-config") {
-                    currentTab = {
-                        heading: (<Heading title='Dummy Metrics for the Cluster' />),
-                        content: () => {
-                            return <Metrics />
-                        }
-                    }
-                }
-                else {
-                    currentTab = {
-                        heading: (<Heading title={"Container Instance ID: " + tabdef.data.ec2InstanceId} />),
-                        content: () => {
-                            return <Instance data={tabdef.data} />
-                        }
+                let data = this.getData()
+                let title = data == "CLUSTER" ?
+                    'Dummy Metrics for the Cluster' :
+                    "Container Instance ID: " + tabdef.data.ec2InstanceId
+                
+                currentTab = {
+                    heading: (<Heading title={title} />),
+                    content: () => {
+                        return <Instance getData={this.getData} />
                     }
                 }
             }
@@ -84,13 +89,126 @@ class Instances extends Component {
     }
 }
 
+// TODO: Fix problem where content flips down to the bottom of the screen when you reduce the width of the screen  
 class Instance extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = this.getState([])
+    }
+
+    getState = (defs) => {
+        if(defs == undefined) {
+            defs = this.state.tabdefs
+        }
+        return {
+            parentData: this.props.getData(),
+            tabdefs: defs
+        }
+    }
+
+    loadData = () => {
+
+        // Find out which is the active tab before the tabdefs get flushed.
+        let activeTabIdx = this.state.tabdefs.findIndex(tabdef => {
+            return tabdef.active
+        })
+        if(activeTabIdx == -1) {
+            activeTabIdx = 0
+        }
+
+        // A different instance may have been clicked, which means the instance state would have changed, so get it again.
+        this.state = this.getState()
+
+        // Flush the tabdefs
+        this.state.tabdefs = []
+
+        // Reconstruct the tabdefs
+        if(this.state.parentData == "CLUSTER") {
+            this.state.tabdefs.push({
+                data: "CLUSTER METADATA",
+                label: "Cluster Metadata",
+                active: activeTabIdx==0
+            })
     
+            this.state.tabdefs.push({
+                data : "CLUSTER DATA",
+                label : "Cluster data",
+                active: activeTabIdx==1
+            })
+        }
+        else {
+            this.state.tabdefs.push({
+                data: this.state.parentData,
+                label: "Metadata",
+                active: activeTabIdx==0
+            })
+
+            this.state.tabdefs.push({
+                data: { // This will get passed in as <Metric data={data} /> It's not really content
+                    dummy: true,
+                    query: {
+                        filter: { 
+                            name: "instanceId",
+                            value: this.state.parentData.ec2InstanceId
+                        }
+                    }
+                },
+                label: "Metrics",
+                active: activeTabIdx==1
+            })
+        }
+
+        this.state.tabdefs.push({
+            data: "Dummy metrics content here",
+            label: "Dummy Metrics",
+            active: activeTabIdx==2
+        })
+    }
+
+    switchTabs = (index) => {
+        let defs = this.state.tabdefs.map((def, idx) => {
+            return {
+                data : def.data,
+                label : def.label,
+                active : (idx === index)
+            }
+        })
+        this.setState(this.getState(defs))
+        return defs
+    }
+
     render() {
-        let json = this.props.data
+
+        this.loadData()
+
+        let currentTab = null;
+        this.state.tabdefs.forEach(tabdef => {
+            if(tabdef.active) {
+                currentTab = {
+                    heading: (<Heading title={tabdef.label} />),
+                    content: () => {
+                        return <JsonToTable json={tabdef.data} />
+                    }
+                }
+            }
+        });
+         
         return (
-            <div className="instanceinfo">
-                <JsonToTable json={json} />                
+            <div style={{paddingLeft:20, paddingTop:30}}>
+                <TabStrip 
+                    defaultTabdefs={this.state.tabdefs} 
+                    refreshTabdefs={this.switchTabs} 
+                    getTabLabel={this.getTabLabel} />
+
+                {/* { currentTab.heading }  */}
+
+{/* RESUME NEXT: currentTab.content() is refreshed when this render() function is invoked by a the parent components render() function. 
+However, the Tabstrip itself does not seem to refresh because its render() method invokes, but with what seems like a cache of old data.
+If this components tabstrip was clicked directly, then that suspected cache is cleared before the tabstrip is re-rendered */}
+                <div className="instanceinfo">
+                    { currentTab.content() }
+                </div>
             </div>
         )
     }
